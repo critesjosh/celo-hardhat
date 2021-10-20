567b972002dd33d2731cd70b67c7249c2e045ede
Josh <critesjosh@gmail.com>
2021-10-20 15:00:32 -0400

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,15 @@ require('dotenv').config({path: '.env'});
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
+// Prints the Celo accounts associated with the mnemonic in .env
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
